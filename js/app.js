0f5c3e29f845ee80a8eea904903f7a0f44e5b122
Michal Grzelczak <mzg.kgb@gmail.com>
2019-04-06 18:18:15 +0200

    Working BookSearch

--- js/app.js
+++ js/app.js
@@ -1,32 +1,33 @@
 $(function () { 
 
-
-    const booksGallery = $(".books-gallery");
-    const booksForm = $(".books-form");
-    const booksInput = $(".books-form-search")
+    const $booksGallery = $(".books-gallery");
+    const $booksForm = $(".books-form");
+    const $booksInput = $(".books-form-search");
+    
     let startIndex = 0;
     let maxResults = 12;
+    let resizeable = true;
 
 
-    booksForm.submit(function (e) {
-        startIndex = 0
-        booksGallery.empty(); 
+    $booksForm.submit(function (e) {
+        startIndex = 0;
+        $booksGallery.empty(); 
         e.preventDefault();
-        const searchTerm = booksInput.val();
+        const searchTerm = $booksInput.val();
         if (searchTerm === ''){
-            console.log('Podaj tytuł');
+            console.log("Type book's title");
         } else {
-            loadBooks(searchTerm, startIndex, maxResults)
-            startIndex = startIndex + maxResults
+            loadBooks(searchTerm, startIndex, maxResults);
         };
     });
 
-    window.onscroll = getMoreBookData // adding more books at the end of the page
 
+    $(window).scroll(getMoreBookData); // adding more books to gallery
+    
 
     function loadBooks(searchTerm , startIndex, maxResults) {
         $.ajax({
-            url: `https://www.googleapis.com/books/v1/volumes?q=intitle:${searchTerm}` ,
+            url: `https://www.googleapis.com/books/v1/volumes?q=intitle:${searchTerm}`,
             method: 'GET',
             data: {
                 startIndex: startIndex,
@@ -34,20 +35,13 @@ $(function () {
             }
         })
         .done(function(response) {
-            // testy
-            console.log('start index: ' + startIndex,'maxresult: '+ maxResults)
-            console.log(response)
-            console.log('wszystkie itemy:' + response.totalItems)
-            console.log('search term: ' + searchTerm)
-            getBooksInfo(response)
-
-            console.log('wysokosc galerii: '+booksGallery.outerHeight(true)) 
-            console.log('wysokosc na stronie: '+ (window.pageYOffset + window.innerHeight))
-
+            getBooksInfo(response);
+            resizeable = true;
         }).fail(function(){
-            console.log("NO DATA")
+            console.log("NO DATA");
         });
     }
+
 
     function getBooksInfo(array) {
         $.each(array.items, function (i, value) { 
@@ -55,38 +49,40 @@ $(function () {
             let title = getBookTitle(value);
             let description = getBookDescription(value);
 
-            populateGallery(cover, title, description)
+            populateGallery(cover, title, description);
         });
     } 
 
+
     function getBookCover(value) {
         if (value.volumeInfo.imageLinks !== undefined) {
-            return value.volumeInfo.imageLinks.thumbnail
+            return value.volumeInfo.imageLinks.thumbnail;
         } else {
-            return "images/NO_IMG.png"
+            return "images/NO_IMG.png";
         } 
     }
 
+
     function getBookTitle(value) {
-        return value.volumeInfo.title
+        return value.volumeInfo.title;
     }
 
 
     function getBookDescription(value) {
         if (value.volumeInfo.description !== undefined) {
             if (value.volumeInfo.description.length > 120) {
-                return value.volumeInfo.description.slice(0,120)+'...'
+                return value.volumeInfo.description.slice(0,120)+'...';
             } else {
-                return value.volumeInfo.description
+                return value.volumeInfo.description;
             }
         } else {
-            return "NO DESCRIPTION AVAILABLE"
+            return "NO DESCRIPTION AVAILABLE";
         }
     } 
 
 
     function populateGallery (cover, title, description) {
-        booksGallery.append(
+        $booksGallery.append(
             `<div class ="books-gallery-book">
                 <div class="book-img">
                     <img src=${cover}/>
@@ -97,18 +93,13 @@ $(function () {
             );
     }
 
-    function getMoreBookData() {
-        let heightBookGallery = Math.round(booksGallery.outerHeight(true)/100);
-        let yOffset = window.pageYOffset;
-        let y = Math.round((yOffset + window.innerHeight)/10);
-        let executed = true
 
-        if (y >= heightBookGallery && executed === false) {
-            // loadBooks(booksInput.val(), startIndex, maxResults)
-            startIndex = startIndex + maxResults
-            executed = true
-            console.log('y:'+ y, 'bookgal: ' +heightBookGallery, 'yoffset: '+yOffset)
-        } 
+    function getMoreBookData() {
+        if (($(window).scrollTop() + $(window).height() > $booksGallery.outerHeight(true)-100) && resizeable) {
+            startIndex = startIndex + maxResults;
+            loadBooks($booksInput.val(), startIndex, maxResults);
+            resizeable = false
+        }
     }
 
  })
